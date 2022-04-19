e119339ad8110436088fd1c68ab1c3ddde255a16
biubiubiupiu21 <94020092+biubiubiupiu21@users.noreply.github.com>
2022-04-18 22:48:57 -0700

    project page

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,8 +1,13 @@
 function projectButton(buttonName){
-    alert(buttonName)
     var page = document.querySelectorAll(".projectText");
+    var buttonlist = document.querySelectorAll(".projectButtons li button");
     for (let i = 0; i < page.length; i++) {
-        document.querySelectorAll(".projectText")[i].style.display = "none"
+        console.log(buttonlist[i].firstChild.nodeValue, buttonName)
+        if (buttonlist[i].firstChild.nodeValue === buttonName) {
+            document.querySelectorAll(".projectText")[i].style.display = "block"
+          }
+        else{
+            document.querySelectorAll(".projectText")[i].style.display = "none"
+        }
     }
-    
 }
